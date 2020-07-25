8a4e427e151384d24026328af1188614edd14c69
SpitfireX <timm.weber@me.com>
2020-07-25 21:59:02 +0200

    testclient now prints formatted responses

--- src/bin/testclient.rs
+++ src/bin/testclient.rs
@@ -1,5 +1,4 @@
-use cqi_rs::{CQiConnection, WriteCQiBytes, ReadCQiBytes};
-use cqi_rs::{BOOL, BYTE, WORD, INT, STRING};
+use cqi_rs::*;
 use std::io::Result as IoResult;
 use cqi_rs::cqi_consts::*;
 use num_traits::FromPrimitive;
@@ -58,61 +57,114 @@ fn main() -> IoResult<()> {
 }
 
 fn process_line(connection: &mut CQiConnection, line: &String) -> IoResult<()>{    
-    let mut bytes: Vec<u8> = vec!();
+    let mut cqi_data = vec!();
 
     for token in line.split_ascii_whitespace() {
         match parse_token(token) {
-            Some(x) => bytes.extend(x.iter()),
+            Some(data) => cqi_data.push(data),
             None => {
-                println!("Could not parse token {}", token);
-                break;
+                println!("Could not parse token \"{}\"", token);
+                return Ok(());
             }
         }
     }
 
-    if bytes.len() > 0 {
-        println!("Sending {} bytes: {:X?}", &bytes.len(), &bytes);
-        connection.stream.write(&bytes)?;
+    if cqi_data.len() > 0 {
+        println!("Sending {} CQi data object(s): {:?}", cqi_data.len(), cqi_data);
+        
+        for data in cqi_data {
+            connection.write_boxed(data)?;
+        }
 
-        let mut buf = [0; 1000];
-        connection.stream.read(&mut buf)?;
-        println!("Response: {:X?}", &buf[..]);
+        let mut buf: Vec<u8> = vec![0; 2];
+        connection.stream.read_exact(&mut buf)?;
+        print!("Response: ");
+        let mut res_bytes = [0; 2];
+        res_bytes.copy_from_slice(&buf[..2]);
+        
+        let datatype = parse_response(u16::from_be_bytes(res_bytes));
 
-        let mut f = File::create("response.bin")?;
-        f.write_all(&buf)?;
+        match datatype {
+            Some(datatype) => {
+                let data: Box<dyn CQiData> = match datatype {
+                    DATA::BYTE => Box::new(connection.read::<BYTE>()?),
+                    DATA::BOOL => Box::new(connection.read::<BOOL>()?),
+                    DATA::INT => Box::new(connection.read::<INT>()?),
+                    DATA::STRING => Box::new(connection.read::<STRING>()?),
+                    DATA::BYTE_LIST => Box::new(connection.read::<BYTE_LIST>()?),
+                    DATA::BOOL_LIST => Box::new(connection.read::<BOOL_LIST>()?),
+                    DATA::INT_LIST => Box::new(connection.read::<INT_LIST>()?),
+                    DATA::STRING_LIST => Box::new(connection.read::<STRING_LIST>()?),
+                    _ => panic!("The fuck is this?"),
+                };
+                println!(" {:?}", data);
+            },
+            None => println!(),
+        }
+
+        // kinda not working right now
+        // let mut f = File::create("response.bin")?;
+        // f.write_all(&buf)?;
+        
     }
 
     Ok(())
 }
 
-fn parse_response(r: WORD) {
+fn parse_response(r: WORD) -> Option<DATA>{
     match ResponseType::from_u8((r >> 8) as u8) {
-        Some(ResponseType::STATUS) => println!("STATUS {:?}", STATUS::from_u16(r).unwrap()),
-        Some(ResponseType::ERROR) => println!("ERROR {:?}", ERROR::from_u16(r).unwrap()),
-        Some(ResponseType::DATA) => println!("DATA {:?}", DATA::from_u16(r).unwrap()),
-        Some(ResponseType::CL_ERROR) => println!("CL_ERROR {:?}", CL_ERROR::from_u16(r).unwrap()),
-        Some(ResponseType::CQP_ERROR) => println!("CQP_ERROR {:?}", CQP_ERROR::from_u16(r).unwrap()),
-        None => println!("0x{:X} Not a valid response type!", (r >> 8) as u8),
+        Some(ResponseType::STATUS) => {
+            print!("STATUS {:?}", STATUS::from_u16(r).unwrap());
+            None
+        },
+        Some(ResponseType::ERROR) => {
+            print!("ERROR {:?}", ERROR::from_u16(r).unwrap());
+            None
+        },
+        Some(ResponseType::DATA) => {
+            match DATA::from_u16(r) {
+                Some(datatype) => {
+                    print!("DATA {:?}", datatype);
+                    Some(datatype)
+                },
+                None => {
+                    print!("DATA UNKNOWN_TYPE");
+                    None
+                }
+            }
+
+        },
+        Some(ResponseType::CL_ERROR) =>{
+            print!("CL_ERROR {:?}", CL_ERROR::from_u16(r).unwrap());
+            None
+        },
+        Some(ResponseType::CQP_ERROR) => {
+            print!("CQP_ERROR {:?}", CQP_ERROR::from_u16(r).unwrap());
+            None
+        },
+        None => {
+            print!("0x{:X} Not a valid response type!", (r >> 8) as u8);
+            None
+        },
     }
 }
 
-fn parse_token(token: &str) -> Option<Vec<u8>> {
-    println!("frag: {}", token);
-    if let Some(bytes) = parse_command(token) { return Some(bytes); }
-    if let Some(bytes) = parse_num_type(token) { return Some(bytes); }
-    if let Some(bytes) = parse_string(token) { return Some(bytes); }
+fn parse_token(token: &str) -> Option<Box<dyn WriteCQiBytes>> {
+    if let Some(data) = parse_command(token) { return Some(data); }
+    if let Some(data) = parse_num_type(token) { return Some(data); }
+    if let Some(data) = parse_string(token) { return Some(data); }
     None
 }
 
-fn parse_command(token: &str) -> Option<Vec<u8>> {
+fn parse_command(token: &str) -> Option<Box<WORD>> {
     let command = token.parse::<COMMANDS>();
     match command {
-        Ok(x) => Some(Vec::from((x as WORD).to_be_bytes())),
+        Ok(x) => Some(Box::new(x as WORD)),
         Err(_) => None,
     }
 }
 
-fn parse_num_type(token: &str) -> Option<Vec<u8>> {
+fn parse_num_type(token: &str) -> Option<Box<dyn WriteCQiBytes>> {
     let frags: Vec<&str> = token.split(":").collect();
 
     if frags.len() >= 1 {
@@ -134,34 +186,31 @@ fn parse_num_type(token: &str) -> Option<Vec<u8>> {
     None
 }
 
-fn parse_byte(token: &str, radix: u32) -> Option<Vec<u8>> {
+fn parse_byte(token: &str, radix: u32) -> Option<Box<dyn WriteCQiBytes>> {
     if let Ok(value) = u8::from_str_radix(token, radix) {
-        return Some(vec!(value))
+        return Some(Box::new(value as BYTE))
     }
     None
 }
 
-fn parse_word(token: &str, radix: u32) -> Option<Vec<u8>> {
+fn parse_word(token: &str, radix: u32) -> Option<Box<dyn WriteCQiBytes>> {
     if let Ok(value) = u16::from_str_radix(token, radix) {
-        return Some(value.to_be_bytes().to_vec())
+        return Some(Box::new(value as WORD))
     }
     None
 }
 
-fn parse_int(token: &str, radix: u32) -> Option<Vec<u8>> {
+fn parse_int(token: &str, radix: u32) -> Option<Box<dyn WriteCQiBytes>> {
     if let Ok(value) = i32::from_str_radix(token, radix) {
-        return Some(value.to_be_bytes().to_vec())
+        return Some(Box::new(value as INT))
     }
     None
 }
 
-fn parse_string(token: &str) -> Option<Vec<u8>> {
+fn parse_string(token: &str) -> Option<Box<STRING>> {
     if token.starts_with("\"") && token.starts_with("\"") {
-        let mut bytes = vec!();
         let len = token.len();
-        bytes.extend((token[1..len-1].len() as u16).to_be_bytes().iter());
-        bytes.extend(token[1..len-1].as_bytes());
-        Some(bytes)
+        Some(Box::new(token[1..len-1].to_owned() as STRING))
     } else {
         None
     }
